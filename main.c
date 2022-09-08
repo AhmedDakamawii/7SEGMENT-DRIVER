ac4fe640d3c10dbfe3463fd446cf8ea7a9025a60
AhmedDakamawii <104605314+AhmedDakamawii@users.noreply.github.com>
2022-09-08 13:44:56 +0200

    Add files via upload
    main.c file is just for testing

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "BIT_MATH.h"
+#include "STD_TYPES.h"
+#include "SEVENSEG_INTERFACE.h"
+#include "DIO_INTERFACE.h"
+
+
+void main(void)
+{
+DIO_VidsetpinDirection(2,3,1);
+SEVSEGMENT_VidDisplayment(0,3);
+
+while (1)
+{
+
+}
+
+
+}
